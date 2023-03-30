85409c69be9f0173e181bb2ec5009ffd100a4632
GoldenWinter <35295993+kevinxcw@users.noreply.github.com>
2023-03-30 21:46:14 +0800

    lib Update

--- src/plugins/lib/config.py
+++ src/plugins/lib/config.py
@@ -4,6 +4,21 @@
 import json
 
 welcome_word = """你好，我是ACM_Club的Q群机器人，很高心能为你服务"""
+
+register_help = """命令格式:
+#register [姓名] [学号]
+姓名和学号用一个空格隔开(不要输入[])
+例：#register 张三 1234567890
+注意：一定要用真实姓名学号，若发现用虚假或别人的，会直接删除所有数据"""
+
+unregister_help = """命令格式:
+#unregister [姓名] [学号]
+姓名和学号用一个空格隔开(不要输入[])
+例：#unregister 张三 1234567890
+注意：一旦注销账户，将会删除该账户的所有数据，不能找回"""
+
+task_help = """123"""
+
 class ConfigClass: # 数据库配置类
     def __init__(self):
         self.database = {
--- src/plugins/lib/databaseclass.py
+++ src/plugins/lib/databaseclass.py
@@ -3,10 +3,11 @@ from ..lib.config import ConfigClass
 import datetime
 
 class Problem: # 数据库题目类
-    url: str
     id: int
     name: str
-    number: str
+    website: str
+    index: str
+    url: str
 
     def __init__(self, url: str, id: int, name: str, number: str):
         self.url = url
@@ -32,28 +33,28 @@ class Score: # 数据库成绩类
         self.penalty = penalty
 
 class Contest: # 数据库比赛类
-    contest_id: int
-    contest_name: str
+    id: int
+    name: str
     time: str
     duration: str
 
-    def __init__(self, contest_id: int, contest_name: str, time: datetime, duration: datetime):
-        self.contest_id = contest_id
-        self.contest_name = contest_name
+    def __init__(self, id: int, name: str, time: datetime, duration: datetime):
+        self.id = id
+        self.name = name
         self.datetime = datetime
         self.duration = duration
 
 class Mission: # 数据库任务类
-    mission_id: int
-    mission_name: str
+    id: int
+    name: str
     description: str
-    next_mission_id: int
+    next_id: int
 
-    def __init__(self, mission_id: int, mission_name: str, description: str, next_mission: int):
-        self.mission_id = mission_id
-        self.mission_name = mission_name
+    def __init__(self, id: int, name: str, description: str, next_id: int):
+        self.id = id
+        self.name = name
         self.description = description
-        self.next_mission_id = next_mission
+        self.next_id = next_id
 
 class task: # 数据库任务类
     mission_id: int
@@ -64,12 +65,16 @@ class task: # 数据库任务类
         self.problem_id = problem_id
 
 class User: # 数据库用户类
+    id: int
     real_name: str
     qq: str
     student_id: str
     codeforces_id: str
+    mission_id: int
+    mission_name: str
 
-    def __init__(self, real_name: str, qq: str, student_id: str, codeforces_id: str = ""):
+    def __init__(self, real_name: str, qq: str, student_id: str, codeforces_id: str = "", id: int = 0):
+        self.id = id
         self.real_name = real_name
         if not self.check_qq(qq):
             raise ValueError("qq账号格式不正确")
@@ -94,7 +99,11 @@ class User: # 数据库用户类
         return True
 
     def to_string(self) -> str:
-        return f"姓名: {self.real_name}\nqq号: {self.qq}\n学号: {self.student_id}\nCodeforces账号: {self.codeforces_id}"
+        return f"""姓名: {self.real_name}
+qq号: {self.qq}
+学号: {self.student_id}
+Codeforces账号: {self.codeforces_id}
+当前的任务:{self.mission_name}"""
 
 
 class DataBase:
@@ -135,19 +144,19 @@ class UserTable(DataBase):
 
     def insert(self, user: User): # users插入，无返回值
         sql = f"insert into users(realname, qq, stuid, codeforces) values('{user.real_name}', '{user.qq}', '{user.student_id}', '{user.codeforces_id}')"
-        self.exec(sql, "In function users_insert users insert failed")
+        self.exec(sql, "In class UserTable function insert users insert failed")
 
     def delete(self, user: User): # users删除，返回删除行数
         sql = f"delete from users where realname='{user.real_name}' and qq='{user.qq}' and stuid='{user.student_id}'"
-        return self.exec(sql, "In function users_delete users delete failed").rowcount
+        return self.exec(sql, "In class UserTable function delete users delete failed").rowcount
 
     def find_all(self): # users查询，返回所有实例
         sql = f"select * from users"
-        return self.exec(sql, "In function users_find_all").fetchall()
+        return self.exec(sql, "In class UserTable function find_all").fetchall()
 
     def find_stuid(self, student_id: str): # users学号查询，返回所有实例
         sql = f"select * from users where stuid='{student_id}'"
-        cursor = self.exec(sql, "In function users_find_stuid users select failed")
+        cursor = self.exec(sql, "In class UserTable function find_stuid users select failed")
         result = cursor.fetchone()
         columns = [col[0] for col in cursor.description]
         if result:
@@ -157,7 +166,7 @@ class UserTable(DataBase):
 
     def find_qq(self, qq: str): # users qq号查询，返回所有实例
         sql = f"select * from users where qq='{qq}'"
-        cursor = self.exec(sql, "In function users_find_qq users select failed")
+        cursor = self.exec(sql, "In class UserTable function find_qq users select failed")
         result = cursor.fetchone()
         columns = [col[0] for col in cursor.description]
         if result:
@@ -165,12 +174,55 @@ class UserTable(DataBase):
         else:
             return None
 
-    def find_realname(self, real_name: str):# users姓名查询，返回所有实例
+    def find_realname(self, real_name: str): # users姓名查询，返回所有实例
         sql = f"select * from users where realname='{real_name}'"
-        return self.exec(sql, "In function users_find_realname users select failed").fetchall()
+        return self.exec(sql, "In class UserTable function find_realname users select failed").fetchall()
 
     def update(self, id: int, user: User): # users更新
         sql = f"update users set realname='{user.real_name}', qq='{user.qq}', stuid='{user.student_id}', codeforces='{user.codeforces_id}' \
         where userid={id}"
-        return self.exec(sql).rowcount > 0
+        return self.exec(sql, "In class UserTable function update user update error").rowcount > 0
 
+class MissionTable(DataBase):
+    def __init__(self):
+        super(Mission, self).__init__()
+
+    def insert(self, mission: Mission):
+        sql = f"insert into missions (missionid, missionname, description, nextmissionid) values ({mission.id}, '{mission.name}', '{mission.description}', {mission.next_id});"
+        self.exec(sql, "In class MissionTable function insert mission insert error")
+
+    def delete(self, mission: Mission):
+        sql = f"delete from misssions where missionid={mission.id}";
+        return self.exec(sql, "In class MissionTable function delete mission delete error").rowcount;
+
+    def update(self, id: int, mission: Mission):
+        sql = f"update users set missionname='{mission.name}', description='{mission.description}, nextmissionid={mission.next_id}' where missionid={id};"
+        return self.exec(sql, "In class MissionTable function update mission update error").rowcount > 0
+
+    def find(self, id: int):
+        sql = f"select * from where missionid={id}"
+        return self.exec(sql, "In class MissionTable function find mission find error").fetchall()
+
+class ProblemTable(DataBase):
+    def __init__(self):
+        super(Promblem, self).__init__()
+
+    def insert(self, problem: Problem):
+        sql = f"insert into problems (problemid, problemname, website, problemindex, url) values ({problem.id}, '{problem.name}', '{problem.website}', '{problem.index}', '{problem.url}');"
+        self.exec(sql, "In class ProblemTable function insert problem insert error")
+
+    def delete(self, problem: Problem):
+        sql = f"delete from problems where problemid={problem.id}"
+        return self.exec(sql, "In class ProblemTable function delete problem delete error")
+
+    def find(self, id: int):
+        sql = f"select * from where problemid={id}"
+        return self.exec(sql, "In class ProblemTable function find problem find error").fetchall()
+
+class TaskTable(DataBase):
+    def __init__(self):
+        super(TaskTable, self).__init__()
+
+    def find(self, id: int):
+        sql = f"select * from where problemid={id}"
+        return self.exec(sql, "In class TaskTable function find task find error").fetchall()
