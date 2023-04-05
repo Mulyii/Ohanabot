8b2ead02ce188fdaa8a3b8ffe3ec84e46c0a1b89
GoldenWinter <35295993+kevinxcw@users.noreply.github.com>
2023-04-05 21:14:26 +0800

    Add files via upload

--- lib/dependclass.py
+++ lib/dependclass.py
@@ -2,7 +2,10 @@
 # 这个Class将MessageEvent转换成一个处理
 # 过的字典
 ####################################
+import datetime
+from nonebot.adapters.onebot.v11.message import Message
 from nonebot.adapters.onebot.v11 import MessageEvent
+from lib.databaseclass import InteractionMessage, InteractionTable
 
 
 class DependClass:
@@ -11,6 +14,7 @@ class DependClass:
     message: str           # QQ消息
     type: str              # 群消息”group“， 私聊消息"private"
     title: str             # 触发头
+    come_from: str         # 消息来源
 
     def __init__(self, event: MessageEvent):
         self.uid = str(event.get_user_id())
@@ -24,4 +28,26 @@ class DependClass:
         self.message = s[i:].strip()
         self.title = s[:i].strip()
         self.type = event.message_type
+        interaction = None
+        if self.type == 'group':
+            _, group_id, user_id = event.get_session_id().split("_")
+            self.come_from = group_id
+            interaction = InteractionMessage(datetime.datetime.now(), user_id, 'bot', 'group', group_id, self.message, self.title)
+        elif self.type == 'private':
+            user_id = event.get_session_id()
+            self.come_from = user_id
+            interaction = InteractionMessage(datetime.datetime.now(), user_id, 'bot', 'private', user_id, self.message, self.title)
+
+        interaction_table = InteractionTable()
+        interaction_table.insert(interaction)
+
+
+async def response(receiver, mes: str, depend: DependClass):
+    interaction = InteractionMessage(datetime.datetime.now(), 'bot', depend.uid,  depend.type, depend.come_from, mes, depend.title)
+    interaction_table = InteractionTable()
+    interaction_table.insert(interaction)
+    if depend.type == "private":
+        await receiver.finish(Message(f'{mes}'))
+    elif depend.type == "group":
+        await receiver.finish(Message(f'[CQ:at,qq={depend.uid}] {mes}'))
 
