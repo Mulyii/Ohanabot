c705872f2fa133c1517ea6c2f09fe7a62d989bab
sunsea409 <97893461+sunsea409@users.noreply.github.com>
2023-04-17 22:29:53 +0800

    增加查看排名插件（查看时需要保证数据库中user有codeforceid）

---
+++ src/plugins/check_rank/__init__.py
@@ -0,0 +1,24 @@
+from nonebot import on_command
+from lib.dependclass import DependClass
+from nonebot.params import Depends
+from lib.codeforcesAPI import *
+from lib.to_picture import *
+from lib.dependclass import DependClass
+from nonebot.adapters.onebot.v11.message import Message, MessageSegment
+
+check_rank = on_command(
+    "#rating",
+    aliases={'查看排名'},
+    priority=1,
+    block=True,
+)
+@check_rank.handle()
+async def rating(user: DependClass = Depends(DependClass, use_cache=False)):
+    sorted_user_rating = get_user_rating()
+    list = []
+    print(sorted_user_rating)
+    for user_rating in sorted_user_rating:
+        user_id, rating = user_rating
+        list.append([user_id, str(rating)])
+    # print(list)
+    await check_rank.finish(MessageSegment.image(await table_to_pic("ZJGSUcodeforce 排行", ["codeforce id","rating"], list)))
