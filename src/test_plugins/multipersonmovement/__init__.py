202ce3309b19b5cd42b25ac259939b3ac096dc9b
徐晓染 <466010106@qq.com>
2023-04-21 01:51:51 +0800

    更新多人比赛，存在的问题 1. 数据库未创建，在线等，join功能待测 2. 现在的题目非随机，为写死状态 3. 比赛时长暂时无法修改，之后考虑更新该功能（两种方案，1. 做为创建比赛时的参数；2. 仅限管理员可以设置）

--- src/test_plugins/multipersonmovement/__init__.py
+++ src/test_plugins/multipersonmovement/__init__.py
@@ -1,42 +1,30 @@
-import re
-import shlex
 import asyncio
-from io import BytesIO
-from asyncio import TimerHandle
-from dataclasses import dataclass
-from typing import Dict, List, Optional, NoReturn, Union
-
-from nonebot.matcher import Matcher
-from nonebot import on_command, on_shell_command, on_message
-from nonebot.params import (
-    ShellCommandArgv,
-    CommandArg,
-    CommandStart,
-    EventPlainText,
-    EventToMe,
-    Depends
-)
-
-from nonebot.adapters.onebot.v11.message import Message, MessageSegment
-from lib.databaseclass import Problem, User, UserTable
-from nonebot.params import CommandArg
-from nonebot.matcher import Matcher
-import lib.codeforcesAPI as cf
 import datetime
+from asyncio import TimerHandle
+from typing import List
 
+from nonebot.matcher import Matcher
+from nonebot import on_command
+from nonebot.params import CommandArg, Depends
+from nonebot.adapters.onebot.v11.message import Message, MessageSegment
+
+import lib.codeforcesAPI as cf
+from lib.databaseclass import Problem, User, UserTable
 from lib.to_picture import table_to_pic
 from lib.dependclass import DependClass
 
+
+
 multi_person_movement = on_command(
     "mpm",
-    aliases={'多人运动', 'multi_person_movement'},
+    aliases={'多人运动', 'multi_person_movement', '多人比赛'},
     priority=5,
     block=True
 )
 
 
 class mpm:
-    def __init__(self, problem: Problem, begin_time = datetime.datetime.now()):
+    def __init__(self, problem: Problem, begin_time=datetime.datetime.now()):
         self.problem = problem
         self.joinPerson: List[User] = []
         self.begin_time = begin_time
@@ -57,12 +45,18 @@ def mpm_running():
 
 
 def check_accept(user: User):
+    global contest
+    if contest == None:
+        return False
     return cf.check_submission(user.codeforces_id, contest.problem.write(), contest.begin_time, datetime.datetime.now())
 
 
 async def create_mpm(matcher: Matcher, dif: int = 1500):
-    contest(cf.random_rating_problem(dif))
-    await matcher.send(f'创建了一场比赛，题目难度为{dif}，链接为{contest.info()}, 输入#join可以加入这场比赛')
+    global contest
+    # contest = mpm(cf.random_rating_problem(dif))
+    contest = mpm(Problem("Timber", 1821, 'F',
+                  'https://codeforces.com/problemset/problem/1821/F'))
+    await matcher.send(f'创建了一场比赛，题目难度为{dif}，链接为:{contest.info()}, 输入#join可以加入这场比赛')
 
 
 async def stop_mpm(matcher: Matcher):
@@ -75,7 +69,7 @@ async def stop_mpm(matcher: Matcher):
         table.append([user.qq, state])
     contest = None
     timer = None
-    await matcher.finish(MessageSegment.image(await table_to_pic(f"比赛结果", headers, table)))
+    await matcher.finish(Message(f'比赛结束，结果如下') + MessageSegment.image(await table_to_pic(f"比赛结果", headers, table)))
 
 
 def set_timeout(matcher: Matcher, timeout: float = 3600):
@@ -89,13 +83,14 @@ def set_timeout(matcher: Matcher, timeout: float = 3600):
 
 @multi_person_movement.handle()
 async def _(matcher: Matcher, arg: Message = CommandArg()):
+    global contest
     if contest != None:
-        matcher.finish(f"现在正在进行的比赛是：{contest.info()}, 你可以输入#join加入这场比赛")
+        await matcher.finish(f"现在正在进行的比赛是：{contest.info()}, 你可以输入#join加入这场比赛")
     dif = arg.extract_plain_text().strip()
     if not dif:
         await create_mpm(matcher)
     elif not dif.isdigit():
-        await matcher.finish(f'输入一个合法的难度')
+        await matcher.finish(f'请输入一个合法的难度')
     else:
         dif = (int(dif) // 100) * 100
         if dif < 800:
@@ -103,13 +98,18 @@ async def _(matcher: Matcher, arg: Message = CommandArg()):
         elif dif > 3500:
             dif = 3500
         await create_mpm(matcher, dif)
-    set_timeout(matcher)
+    set_timeout(matcher, 10)
 
-join = on_command("join")
+join = on_command(
+    "join",
+    priority=5,
+    block=True
+)
 
 
 @join.handle()
 async def _(qq_account: DependClass = Depends(DependClass, use_cache=False)):
+    global contest
     if contest == None:
         join.finish(f'没有正在进行的比赛，你可以发送#mpm创建一场比赛')
     user = UserTable.find_qq(qq_account)
